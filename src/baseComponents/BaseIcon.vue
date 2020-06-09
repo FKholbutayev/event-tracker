e08445358fd3a7c58c5f826c8b43cf54b705947e
extrahappyperson <ht.uzpharma@gmail.com>
2020-06-09 13:49:52 +0200

    vue-icon-components

---
+++ src/baseComponents/BaseIcon.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <template>
+      <div class="icon-wrapper">
+        <svg class="icon" :width="width" :heigth="heigth">
+          <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }" />
+          />
+        </svg>
+      </div>
+      <feather-icon type="activity"></feather-icon>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    heigth: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+const VueFeatherIcon = require('vue-feather-icon')
+
+Vue.use(VueFeatherIcon)
+
+import BaseIcon from '@/baseComponents/BaseIcon'
+
+//globally register base components
+Vue.component('BaseIcon', BaseIcon)
+//Vue.component('BaseButton', BaseButton)
+//Vue.component('BaseInput', BaseInput)
 
 Vue.config.productionTip = false
 
